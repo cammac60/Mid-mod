a160243496e304a5402a1383df06110f3d641327
Cameron MacRae <cam.mac.cm@gmail.com>
2019-12-11 10:03:07 -0700

    Add cards

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from '../Card/Card.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/v1/reservations')
+    .then(response => response.json())
+    .then(data => this.setState({reservations: [...this.state.reservations, data]}))
+    .catch(err => console.log(err))
+  }
+
   render() {
+    let cards;
+    if (this.state.reservations.length) {
+      cards = this.state.reservations[0].map(res => {
+        return ( <Card name={res.name} key={res.id}/> )
+      });
+    }
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
@@ -10,7 +31,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          {cards}
         </div>
       </div>
     )
